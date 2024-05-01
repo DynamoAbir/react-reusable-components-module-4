4a71001cec0e17c89d17fa2c48d22e251022b80e
DynamoAbir <mhabir10@gmail.com>
2024-05-02 00:49:16 +0600

    first commit

---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <div className="h-16 w-full bg-purple-400">
+      <h1>This is NavBar component</h1>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <div className="h-screen bg-red-500 ">
+      <h1>This is Sidebar component</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
